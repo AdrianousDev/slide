ebdedc5594cf9ce902f527ad450a9a0b16c7f823
AdrianousDev <brunoadrianousz@gmail.com>
2026-01-31 15:57:26 -0300

    feat: adiciona transition nas animações do slide

--- js/slide.js
+++ js/slide.js
@@ -5,6 +5,10 @@ export default class Slide {
     this.dist = { finalPosition: 0, startX: 0, movement: 0 };
   }
 
+  transition(active) {
+    this.slide.style.transition = active ? "transform .3s" : "";
+  }
+
   moveSlide(distX) {
     this.dist.movePosition = distX;
     this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
@@ -26,6 +30,7 @@ export default class Slide {
       moveType = "touchmove";
     }
     this.wrapper.addEventListener(moveType, this.onMove);
+    this.transition(false);
   }
 
   onMove(event) {
@@ -41,6 +46,18 @@ export default class Slide {
     const moveType = event.type === "mouseup" ? "mousemove" : "touchmove";
     this.wrapper.removeEventListener(moveType, this.onMove);
     this.dist.finalPosition = this.dist.movePosition;
+    this.transition(true);
+    this.changeSlideOnEnd();
+  }
+
+  changeSlideOnEnd() {
+    if (this.dist.movement > 120 && this.index.next) {
+      this.activeNextSlide();
+    } else if (this.dist.movement < 120 && this.index.prev) {
+      this.activePrevSlide();
+    } else {
+      this.changeSlide(this.index.active);
+    }
   }
 
   addSlideEvents() {
@@ -86,11 +103,22 @@ export default class Slide {
     this.dist.finalPosition = activeSlide.position;
   }
 
+  activePrevSlide() {
+    if (!this.index.prev) return;
+    this.changeSlide(this.index.prev);
+  }
+
+  activeNextSlide() {
+    if (!this.index.next) return;
+    this.changeSlide(this.index.next);
+  }
+
   init() {
     if (!this.slide || !this.wrapper) return "error";
     this.bindEvents();
     this.addSlideEvents();
     this.slidesConfig();
+    this.transition(true);
 
     return this;
   }
